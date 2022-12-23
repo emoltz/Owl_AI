9c195b693a9123e10d63b8a3731bc9c9423e74cc
emoltz <es5541@nyu.edu>
2022-12-23 10:12:51 -0500

    basic setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
 import '../styles/globals.css'
+import {Toaster} from "react-hot-toast";
+import {createTheme, NextUIProvider} from '@nextui-org/react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const theme = createTheme({
+    type: "light",
+    theme:{
+        colors: {
+            primary: "#3eaf63",
+            success: "#F9CB80",
+            error: "#FCC5D8",
+            background: "#fafafa",
+
+        }
+    }
+});
+
+export default function App({Component, pageProps}) {
+    return (
+        <>
+            <NextUIProvider theme={theme}>
+                <Component {...pageProps} />
+                <Toaster position={"bottom-right"} reverseOrder={false}/>
+            </NextUIProvider>
+
+        </>
+
+    )
 }
