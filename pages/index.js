6dbb70f52442450efca39c398d89fc191ba0889e
emoltz <es5541@nyu.edu>
2022-12-23 11:59:34 -0500

    added button to test on vercel

--- pages/index.js
+++ pages/index.js
@@ -8,20 +8,18 @@ import toast from "react-hot-toast";
 
 export default function Home() {
     const handleClick = async () => {
-        // const response = await fetch('/api/dream', {
-        //     method: 'POST',
-        //     body: JSON.stringify({
-        //         prompt: "Write a short story"
-        //     }),
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-        // })
-        // const data = await response.json()
-        // console.log(data)
-        const test = process.env.NEXT_PUBLIC_TEST
-        console.log(test)
-        // console.log("HELLO")
+        const response = await fetch('/api/dream', {
+            method: 'POST',
+            body: JSON.stringify({
+                prompt: "Write a short story"
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        console.log(data)
+
     }
 
 
