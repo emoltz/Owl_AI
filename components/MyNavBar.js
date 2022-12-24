9ed8d969bcdc614e784af589739f7b563a4ce843
emoltz <es5541@nyu.edu>
2022-12-24 14:03:14 -0500

    conditional for navbar

--- components/MyNavBar.js
+++ components/MyNavBar.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Navbar, Button, Link, Text, Card, Radio } from "@nextui-org/react";
+import {Navbar, Button, Link, Text, Card, Radio} from "@nextui-org/react";
 import {OwlLogo} from "./OwlLogo";
 
 // Top navbar
 export default function MyNavBar() {
     const user = null;
     const username = null;
+
+    // const user = true;
+    // const username = true;
 
     const [variant, setVariant] = React.useState("floating");
     return (
@@ -22,16 +25,42 @@ export default function MyNavBar() {
 
                 </Navbar.Content>
 
-                <Navbar.Content hideIn={""}>
-                    <Navbar.Link href={"/"}>About</Navbar.Link>
-                    <Navbar.Link href={"/"}>My Stuff</Navbar.Link>
-                    <Navbar.Item>
-                        <Button auto flat as={Link} href={"#"}>
-                            Login or Signup
-                        </Button>
-                    </Navbar.Item>
+                {/*SIGNED IN*/}
+                {username && (
+                <>
+                    <Navbar.Content hideIn={""}>
+                        <Navbar.Link href={"/"}>About</Navbar.Link>
+                        <Navbar.Link href={"/"}>My Stuff</Navbar.Link>
+                        <Navbar.Item>
+                            <Button auto flat as={Link} href={"#"}>
+                                My Profile
+                            </Button>
+                        </Navbar.Item>
 
-                </Navbar.Content>
+                    </Navbar.Content>
+                </>
+
+                )}
+
+                {/*    END SIGNED IN*/}
+
+                {!username && (
+                    <>
+                    <Navbar.Content hideIn={""}>
+                        <Navbar.Item>
+                            <Button auto flat as={Link} href={"#"}>
+                                Login / Sign Up
+                            </Button>
+                        </Navbar.Item>
+
+                    </Navbar.Content>
+                    </>
+                )}
+
+                {/*NOT SIGNED IN*/}
+
+
+                {/*    END NOT SIGNED IN*/}
 
 
             </Navbar>
