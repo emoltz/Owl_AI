c74f7a8fea5291654e5d657c79f57f88c377f0eb
emoltz <es5541@nyu.edu>
2023-01-01 18:07:51 -0500

    small edits to text

--- components/Welcome.js
+++ components/Welcome.js
@@ -3,25 +3,33 @@ import {Card, Spacer, Text, Grid, Col, Row, Button, Container} from "@nextui-org
 import Link from "next/link";
 
 const Card2 = () => (
-    <>
-        <Card isHoverable isPressable>
-            <Card.Header>Welcome to Owl.ai!</Card.Header>
-            <Card.Body> Welcome to our website! We understand that reading can be a challenge for some students, which
-                is why we have developed a tool to help.
+        <>
+            <Card isHoverable isPressable>
+                <Card.Header >
+                    <Text css={{fontWeight: "bold"}}>Welcome to Owl.ai, an auto differentiation tool for educators.</Text>
 
-                Our website takes reading text and uses machine learning to scaffold it to different reading levels.
-                This means that you can input a piece of text and select the desired reading level, and our tool will
-                adjust the text to match that level.
+                </Card.Header>
+                <Card.Body>
 
-                This is especially useful for students who may be struggling with reading comprehension or for educators
-                looking to differentiate instruction for their students.
+                    We understand that reading can be a challenge for some students, which
+                    is why we have developed a tool to help.
 
-                Try it out for yourself and see how our tool can help improve reading skills and confidence.</Card.Body>
-        </Card>
-    </>
+                    Our website takes reading text and uses machine learning to scaffold it to different reading levels.
+                    This means that you can input a piece of text and select the desired reading level, and our tool will
+                    adjust the text to match that level.
+
+                    This is especially useful for students who may be struggling with reading comprehension or for educators
+                    looking to differentiate instruction for their students.
+                </Card.Body>
+                <Card.Footer >
+                    To get started, signup and login above.
+                </Card.Footer>
+            </Card>
+        </>
 
 
-);
+    )
+;
 
 
 function Welcome({}) {
