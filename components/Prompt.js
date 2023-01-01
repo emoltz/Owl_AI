bf86e11615c3ce6ebf04d2c3af16e61a5081ac42
emoltz <es5541@nyu.edu>
2023-01-01 16:46:38 -0500

    drop down menus

--- components/Prompt.js
+++ components/Prompt.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Button, Input} from "@nextui-org/react";
-import {Grid, Container, Card, Spacer, Textarea} from "@nextui-org/react";
+import {Button, Input, Text} from "@nextui-org/react";
+import {Grid, Container, Dropdown, Spacer, Textarea} from "@nextui-org/react";
 import toast from "react-hot-toast";
 
 
@@ -32,6 +32,17 @@ const Prompt = () => {
 
     }
 
+    const [selectedGradeLevel, setSelectedGradeLevel] = React.useState(new Set(["Select Grade Level"]));
+    const selectedGradeLevelValue = React.useMemo(
+        () => Array.from(selectedGradeLevel).join(", ").replaceAll("_", " ")
+        , [selectedGradeLevel]
+    );
+
+    const [selectedLanguage, setSelectedLanguage] = React.useState(new Set(["Select Language"]));
+    const selectedLanguageValue = React.useMemo(
+        () => Array.from(selectedLanguage).join(", ").replaceAll("_", " ")
+        , [selectedLanguage]
+    );
 
     return (
         <>
@@ -51,6 +62,53 @@ const Prompt = () => {
                             fullWidth={true}
 
                         />
+
+                        <Spacer y={1}/>
+                        <Grid.Container justify={"center"}>
+
+
+                            <Dropdown>
+                                <Dropdown.Button flat> {selectedGradeLevelValue}</Dropdown.Button>
+                                <Dropdown.Menu aria-label="Reading Level"
+                                               disallowEmptySelection={true}
+                                               selectionMode={"single"}
+                                               selectedKeys={selectedGradeLevel}
+                                               onSelectionChange={setSelectedGradeLevel}
+                                >
+                                    <Dropdown.Item key={"1st Grade"}>1st Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"2nd Grade"}>2nd Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"3rd Grade"}>3rd Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"4th Grade"}>4th Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"5th Grade"}>5th Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"6th Grade"}>6th Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"7th Grade"}>7th Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"8th Grade"}>8th Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"9th Grade"}>9th Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"10th Grade"}>10th Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"11th Grade"}>11th Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"12th Grade"}>12th Grade</Dropdown.Item>
+                                    <Dropdown.Item key={"College"}>College</Dropdown.Item>
+                                </Dropdown.Menu>
+
+                            </Dropdown>
+                            <Spacer y={1}/>
+                            <Dropdown>
+                                <Dropdown.Button flat> {selectedLanguageValue}</Dropdown.Button>
+                                <Dropdown.Menu aria-label="Language"
+                                               disallowEmptySelection={true}
+                                               selectionMode={"single"}
+                                               selectedKeys={selectedLanguage}
+                                               onSelectionChange={setSelectedLanguage}
+                                >
+                                    <Dropdown.Item key={"English"}>English</Dropdown.Item>
+                                    <Dropdown.Item key={"Spanish"}>Spanish</Dropdown.Item>
+                                    <Dropdown.Item key={"French"}>French</Dropdown.Item>
+                                </Dropdown.Menu>
+
+                            </Dropdown>
+                        </Grid.Container>
+
+
                         <Spacer y={1}/>
                         <Grid.Container justify={"center"}>
                             <Grid alignItems={"center"}>
@@ -58,7 +116,16 @@ const Prompt = () => {
                             </Grid>
                         </Grid.Container>
 
-                        {result && <div id="result"> {result} </div>}
+
+                        {/*RESULTS:*/}
+                        {result &&
+                            <>
+                                <Spacer y={2}/>
+                                <Text blockquote
+                                      id="result"> {result} </Text>
+                            </>
+
+                        }
                     </form>
                 </div>
 
