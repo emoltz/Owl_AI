39e628bfe4e6a35abfceae9ffdbb3557490ba0cd
emoltz <es5541@nyu.edu>
2023-01-16 10:49:08 -0700

    firebase stuff working

--- pages/mystuff.js
+++ pages/mystuff.js
@@ -1,7 +1,7 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Container} from "@nextui-org/react";
-import {collectionGroup, getDocs, getFirestore, orderBy, query, where} from "firebase/firestore";
-import {postToJSON} from "../lib/firebase";
+import {collectionGroup, getDocs, getFirestore, onSnapshot, orderBy, query, where} from "firebase/firestore";
+import {getCurrentUser, postToJSON} from "../lib/firebase";
 import {UserContext} from "../lib/context";
 import {RotateLoader} from "react-spinners";
 import {MyStuffCard} from "../components/MyStuffCard";
@@ -26,23 +26,29 @@ export async function getServerSideProps(context) {
 
 function MyStuff(props) {
     const {user} = useContext(UserContext);
-    // const {userEmail, userDisplayName, userID} = getCurrentUser();
-    const [res, setRes] = useState(props.res);
+    const [data, setData] = useState(null);
+    let testData = null;
+    const {userEmail, userDisplayName, userID} = getCurrentUser();
+    // const [res, setRes] = useState(props.res);
+    const [res, setRes] = useState(null);
 
-    const getMyStuff = async () => {
+    //get users stuff from firebase firestore
+    useEffect(() => {
         const ref = collectionGroup(getFirestore(), 'saved_text')
         const q = query(
             ref,
-            where('user_id', '==', "OTArZphQoKUcN6pHIpSXfB2QUI12"), //TODO make sure this is for the specific user
-            // TODO https://colinhacks.com/essays/nextjs-firebase-authentication
+            where('user_id', '==', userID),
             orderBy('created_date', 'desc')
         )
 
-        return (await getDocs(q)).docs.map(postToJSON);
-    }
-
-
-
+        return onSnapshot(q, (querySnapshot) => {
+            const data = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setRes(data);
+        });
+    })
 
     return (
         <>
@@ -58,19 +64,42 @@ function MyStuff(props) {
                 }
             </Container>
             <Container>
-                {res.map((item) => {
-                    return (
-                        <div key={item.id}>
-                            <MyStuffCard cardLevel={item.grade_level} cardTitle={item.title}
-                                         cardContents={item.contents}
-                                // cardDate={item.created_date}
-                            />
-                        </div>
-                    )
+                {res ?
 
-                })}
+                    res.map((item) => {
+                        return (
+                            <div key={item.id}>
+                                <MyStuffCard cardLevel={item.grade_level} cardTitle={item.title}
+                                             cardContents={item.contents}
+                                    // cardDate={item.created_date}
+                                />
+                            </div>
+                        )
+
+                    })
+                    :
+                    <Container fluid>
+
+                        <RotateLoader color={"#000000"} loading={true} size={20}/>
+                    </Container>
+
+                }
+
 
             </Container>
+
+
+            {/*{data.map((item) => {*/}
+            {/*    return (*/}
+            {/*        <div key={item.id}>*/}
+            {/*            <MyStuffCard cardLevel={item.grade_level} cardTitle={item.title}*/}
+            {/*                         cardContents={item.contents}*/}
+            {/*                // cardDate={item.created_date}*/}
+            {/*            />*/}
+            {/*        </div>*/}
+            {/*    )*/}
+
+            {/*}, [])}*/}
 
         </>
 
