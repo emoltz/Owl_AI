29cb83622bf916c19ca3db20774ef3622cbba57d
emoltz <es5541@nyu.edu>
2023-01-19 20:05:57 -0500

    fixed small error in `useEffect`

--- pages/mystuff.js
+++ pages/mystuff.js
@@ -1,32 +1,35 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {Container} from "@nextui-org/react";
 import {collectionGroup, getDocs, getFirestore, onSnapshot, orderBy, query, where} from "firebase/firestore";
-import {getCurrentUser, postToJSON} from "../lib/firebase";
+import {auth, getCurrentUser, postToJSON} from "../lib/firebase";
 import {UserContext} from "../lib/context";
 import {RotateLoader} from "react-spinners";
 import {MyStuffCard} from "../components/MyStuffCard";
+import {useAuthState} from "react-firebase-hooks/auth";
 
 function MyStuff(props) {
     const user = useContext(UserContext);
     const [res, setRes] = useState(null);
 
-    //get users stuff from firebase firestore
     useEffect(() => {
-        const ref = collectionGroup(getFirestore(), 'saved_text')
-        const q = query(
-            ref,
-            where('user_id', '==', user.uid),
-            orderBy('created_date', 'desc')
-        )
+        if (user?.uid) {
+            const ref = collectionGroup(getFirestore(), 'saved_text')
+            const q = query(
+                ref,
+                where('user_id', '==', user?.uid),
+                orderBy('created_date', 'desc')
+            )
 
-        return onSnapshot(q, (querySnapshot) => {
-            const data = querySnapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data()
-            }));
-            setRes(data);
-        });
-    })
+            return onSnapshot(q, (querySnapshot) => {
+                const data = querySnapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+                setRes(data);
+            });
+        }
+    }, [user])
+
 
     return (
         <>
