d5ba3beaa774f97fdec0b9f213d5abdd1d772a0e
emoltz <es5541@nyu.edu>
2023-01-19 21:08:01 -0500

    serverside rendering for mystuff!

--- pages/mystuff.js
+++ pages/mystuff.js
@@ -1,14 +1,27 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Container} from "@nextui-org/react";
+import {Container, Divider} from "@nextui-org/react";
 import {collectionGroup, getDocs, getFirestore, onSnapshot, orderBy, query, where} from "firebase/firestore";
 import {auth, getCurrentUser, postToJSON} from "../lib/firebase";
 import {UserContext} from "../lib/context";
 import {RotateLoader} from "react-spinners";
 import {MyStuffCard} from "../components/MyStuffCard";
-import {useAuthState} from "react-firebase-hooks/auth";
+
+
+export async function getServerSideProps(context) {
+    const ref = collectionGroup(getFirestore(), 'saved_text')
+    const q = query(
+        ref,
+    )
+    const data = (await getDocs(q)).docs.map(postToJSON);
+    return {
+        props: {data},
+    }
+}
+
 
 function MyStuff(props) {
     const user = useContext(UserContext);
+    const [data, setData] = useState(props.data);
     const [res, setRes] = useState(null);
 
     useEffect(() => {
@@ -30,13 +43,37 @@ function MyStuff(props) {
         }
     }, [user])
 
+    // const ref = collectionGroup(getFirestore(), 'saved_text')
+    // const [posts, loading, error] = useCollection(
+    //     query(ref)
+    // )
+    // if (!loading && posts) {
+    //     posts.docs.map((doc) => {
+    //             if (doc.data().user_id === user?.uid) {
+    //                 console.log(doc.data().user_id);
+    //             }
+    //
+    //         }
+    //     )
+    //     ;
+    // }
+
+    // data.map((doc) => {
+    //         if (doc.user_id === user?.uid) {
+    //             console.log(doc.contents, user?.uid);
+    //
+    //         }
+    //     }
+    // )
+
+    console.log(user?.uid)
 
     return (
         <>
             <Container>
                 {user ?
                     <h1>
-                        Welcome, {user.displayName}! <br/>
+                        Welcome, {user?.displayName}! <br/>
                     </h1>
                     :
                     <Container>
@@ -44,29 +81,26 @@ function MyStuff(props) {
                     </Container>
                 }
             </Container>
+
+
             <Container>
-                {res ?
+                {data ?
+                    data.map((doc) => {
+                        if (doc.user_id === user?.uid) {
+                            return (
+                                <div key={doc.id}>
+                                    <MyStuffCard cardLevel={doc.grade_level} cardTitle={doc.title}
+                                                 cardContents={doc.contents}
+                                    />
+                                </div>
+                            )
+                        }
 
-                    res.map((item) => {
-                        return (
-                            <div key={item.id}>
-                                <MyStuffCard cardLevel={item.grade_level} cardTitle={item.title}
-                                             cardContents={item.contents}
-                                    // cardDate={item.created_date}
-                                />
-                            </div>
-                        )
-
-                    })
-                    :
-                    <Container fluid>
-
-                        <RotateLoader color={"#000000"} loading={true} size={20}/>
-                    </Container>
-
+                    }) :
+                    <div>
+                        Loading...
+                    </div>
                 }
-
-
             </Container>
 
         </>
